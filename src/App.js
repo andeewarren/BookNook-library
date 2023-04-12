c37595af40c30949e7e2340d50644963be1f66dd
Andee Warren <119145298+andeewarren@users.noreply.github.com>
2023-04-12 15:47:14 -0400

    starting w/ReactRouter

--- src/App.js
+++ src/App.js
@@ -1,9 +1,19 @@
+import React from 'react';
+import { BrowserRouter, Route, Routes } from 'react-router-dom';
 import { BookList } from './components/BookList';
+import Library from './routes/library';
 import './App.css';
 
 function App() {
   return (
-    <BookList />
+    <div>
+      <BookList />
+      <BrowserRouter>
+        <Routes>
+          <Route path='/' element={<Library />}></Route>
+        </Routes>
+      </BrowserRouter>
+    </div>
   );
 }
 
